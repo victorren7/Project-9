12d3fbafba5f5ea016e37dd472d26da1e93c2f9f
victor <31532226+victorren7@users.noreply.github.com>
2018-03-11 23:57:36 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,15 @@
-
 import React from 'react';
-import GetImages from './GetImages';
 
- //a welcome page to begin with. Shows random pictures and gives some friendly instructions.
 
   const Home = props =>{
 
     return (
       <div>
       <div className="home">
-        <h2>Welcome To My Animal Gallery</h2>
+        <h2>Welcome To My Gallery</h2>
         <p>Click on any animal button or you can click on on the search button and do your own search</p>
       </div>
 
-
-      <GetImages subject ="random photos"/>
 
       </div>
     )
